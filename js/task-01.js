0d237d81b5bd3e6f43937e1cb3fd54a13a8c8458
Nikita Zelenyak <Zelenyaknikita0927@gmail.com>
2023-10-22 10:53:15 -0400

    fixed

--- js/task-01.js
+++ js/task-01.js
@@ -1,54 +1,12 @@
-const listEl = document.querySelector('#categories');
+const categoriesList = document.getElementById("categories");
+const categories = categoriesList.children;
 
-const itemsEl = listEl.querySelectorAll('.item');
-console.log(`Number of categories: ${itemsEl.length}`);
+console.log("Number of categories:", categories.length);
 
-
-
-
-const arrTitels = [...itemsEl].map(element => element.querySelector('h2'));
-const arrItem = [...itemsEl].map(element => element.querySelectorAll('li'));
-
-console.log(arrTitels[0].textContent)
-console.log(`Elements: ${arrItem[0].length}`)
-
-
-console.log(arrTitels[1].textContent)
-console.log(`Elements: ${arrItem[1].length}`)
-
-
-console.log(arrTitels[2].textContent)
-console.log(`Elements: ${arrItem[2].length}`)
-
-
-// ////////////////////////////////////////////////////////
-
-// const firstTitel = itemsEl[0].querySelector('h2');
-// console.log(firstTitel.textContent);
-// const listLiEl = itemsEl[0].querySelectorAll('li');
-// console.log(`Elements: ${listLiEl.length}`)
-
-// const secondTitel = itemsEl[1].querySelector('h2');
-// console.log(secondTitel.textContent);
-// const secondlistLiEl = itemsEl[1].querySelectorAll('li');
-// console.log(`Elements: ${secondlistLiEl.length}`)
-
-// const thirdTitel = itemsEl[2].querySelector('h2');
-// console.log(thirdTitel.textContent);
-// const thirdlistLiEl = itemsEl[2].querySelectorAll('li');
-// console.log(`Elements: ${thirdlistLiEl.length}`)
-
-
-
-
-
-// itemsEl.forEach(element => {
-//     const titelEl = element.querySelector('h2');
-//     console.log(`Category: ${titelEl.textContent}`)
-    
-// });
-// itemsEl.forEach(element => {
-//     const allListItemEls = element.querySelectorAll('li');
-//     console.log(`Elements: ${allListItemEls.length}`);
-    
-// });
+for (let i = 0; i < categories.length; i++) {
+  const category = categories[i];
+  const categoryName = category.querySelector("h2").textContent;
+  const categoryItems = category.querySelectorAll("li").length;
+  console.log("Category:", categoryName);
+  console.log("Elements:", categoryItems);
+}
--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,14 @@
-const inputEl = document.querySelector('#name-input');
-const changingNameEl = document.querySelector('#name-output');
+const inputEl = document.querySelector("#name-input");
+const changingNameEl = document.querySelector("#name-output");
 
-
-
-inputEl.addEventListener('input',onInputChange);
+inputEl.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
+  const inputValue = event.currentTarget.value.trim();
 
-    changingNameEl.textContent=event.currentTarget.value
-
-    }
+  if (inputValue === "") {
+    changingNameEl.textContent = "Anonymous";
+  } else {
+    changingNameEl.textContent = inputValue;
+  }
+}
